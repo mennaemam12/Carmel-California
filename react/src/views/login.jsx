55e95a46e951016a291bc5a831740b882444f09f
mennaemam12 <mennaemam205@gmail.com>
2023-10-25 23:57:02 +0300

    reg

--- react/src/views/login.jsx
+++ react/src/views/login.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import registrationImage from './../images/reg.jpg';
+import './../css/login.css'; 
 import { Link } from "react-router-dom";
 
 export default function login(){
@@ -5,20 +8,43 @@ export default function login(){
         ev.preventDefault();
     }
 
-    return(
-        <div className="login-signup-form animated fadeInDown">
-            <div className="form">
-                <h1 className="title">Login into your account</h1>
-                <form onSubmit={onSubmit}>
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <button className="btn btn-block">Login</button>
-                    <p className="message">
-                        Not Registered? <Link to="/signup">Create an account </Link>
-                    </p>
+    return (
+        <html>
+          <head>
+            <meta charSet="utf-8" />
+            <title>Login</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="stylesheet" href="css/login.css" />
+          </head>
+          <body>
+            <div className="wrapper">
+              <div className="inner">
+                <div className="image-holder">
+                  <img src={registrationImage} style={{ objectFit: 'fill', maxHeight: '780px' }} alt="" />
+                </div>
+                <form action="">
+                  <h3>Login</h3>
+                  <div className="form-holder active">
+                    <input type="text" placeholder="e-mail" className="form-control" />
+                  </div>
+                  <div className="form-holder">
+                    <input type="password" placeholder="Password" className="form-control" style={{ fontSize: '15px' }} />
+                  </div>
+                  <div className="checkbox" style={{ display: 'none' }}>
+                    <label>
+                      <input type="checkbox" defaultChecked /> I agree to all statements in <a href="#">Terms & Conditions</a>
+                      <span className="checkmark"></span>
+                    </label>
+                  </div>
+                  <div className="form-login" style={{ marginTop: '10%' }}>
+                    <button>Login</button>
+                    <p> Don't have an account?{' '}
+                     <Link to="/signup">Sign Up</Link> </p>
+                  </div>
                 </form>
-
+              </div>
             </div>
-        </div>
-    )
+          </body>
+        </html>
+      );
 }
