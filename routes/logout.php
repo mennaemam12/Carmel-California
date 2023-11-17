184261e62c32b937710a7c9ba7e3bb479f8fa2e6
shahd <shahd2106627@miuegypt.edu.eg>
2023-11-18 01:12:03 +0200

    logout route

---
+++ routes/logout.php
@@ -0,0 +1,18 @@
+<?php
+// Path: routes/login.php
+
+include 'projectFolderName.php';
+
+// Commenting till we have a proper database
+// Check if the user is already logged in
+// if (isset($_SESSION['userType'])) {
+//     header('Location: ' .$projectFolder. '/'); // Redirect to the home page
+//     exit();
+// }
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    include 'controllers/user.controller.php';
+    $user = new UserController;
+    $user->logout();
+}
+
