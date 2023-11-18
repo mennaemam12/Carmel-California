bc51f25a5fd0fac997154fbd5d174f49bbf7aa56
mennaemam12 <mennaemam205@gmail.com>
2023-11-19 00:18:41 +0200

    .

--- carmel-california.sql
+++ carmel-california.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 18, 2023 at 03:23 AM
+-- Generation Time: Nov 18, 2023 at 11:18 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,6 +24,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `breakfast`
+--
+
+CREATE TABLE `breakfast` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `category` varchar(20) NOT NULL,
+  `description` varchar(65) NOT NULL,
+  `price` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dinner`
+--
+
+CREATE TABLE `dinner` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `category` varchar(20) NOT NULL,
+  `description` varchar(65) NOT NULL,
+  `price` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `drinks`
+--
+
+CREATE TABLE `drinks` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `category` varchar(20) NOT NULL,
+  `description` varchar(65) NOT NULL,
+  `price` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `items`
 --
 
@@ -38,6 +80,20 @@ CREATE TABLE `items` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `sides`
+--
+
+CREATE TABLE `sides` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `category` varchar(20) NOT NULL,
+  `description` varchar(65) NOT NULL,
+  `price` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -47,17 +103,49 @@ CREATE TABLE `users` (
   `UserName` varchar(50) NOT NULL,
   `UserPass` varchar(255) NOT NULL,
   `PhoneNumber` int(11) NOT NULL,
-  `id` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `type` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`FullName`, `Email`, `UserName`, `UserPass`, `PhoneNumber`, `id`, `type`) VALUES
+('Menna Emam', 'mennaemaam12@gmail.com', 'mennaemam', '$2y$10$gEEdH30mYNok7SQixvbqSOq5de/394x247bUDNH3JF.4qCmWgqa5W', 1092348337, 26, 'user');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `breakfast`
+--
+ALTER TABLE `breakfast`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dinner`
+--
+ALTER TABLE `dinner`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `drinks`
+--
+ALTER TABLE `drinks`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `items`
 --
 ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sides`
+--
+ALTER TABLE `sides`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -71,16 +159,40 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `breakfast`
+--
+ALTER TABLE `breakfast`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `dinner`
+--
+ALTER TABLE `dinner`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `drinks`
+--
+ALTER TABLE `drinks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `sides`
+--
+ALTER TABLE `sides`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
