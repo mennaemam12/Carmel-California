eaa5374a1104e3fde37a71778b9d60d6b9d52732
Ramez Ehab <ramezehab2@gmail.com>
2023-11-19 22:00:17 +0200

    href fix

--- views/dashboard.php
+++ views/dashboard.php
@@ -31,8 +31,8 @@ include 'projectFolderName.php';
     <!-- partial:partials/_navbar.html -->
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
       <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center" style="background-color: #DFE0DA;">
-        <a class="navbar-brand brand-logo mr-5" href="index.html"><img src="template/images/CARMEL (2).png" class="mr-2" alt="logo" /></a>
-        <a class="navbar-brand brand-logo-mini" href="index.html"><img src="template/images/logo-mini.svg" alt="logo" /></a>
+        <a class="navbar-brand brand-logo mr-5" href="index"><img src="template/images/CARMEL (2).png" class="mr-2" alt="logo" /></a>
+        <a class="navbar-brand brand-logo-mini" href="index"><img src="template/images/logo-mini.svg" alt="logo" /></a>
       </div>
       <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end" style="background-color: #DFE0DA; ">
         <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
@@ -130,8 +130,8 @@ include 'projectFolderName.php';
       <!-- partial:partials/_settings-panel.html -->
       <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center" style="background-color: #DFE0DA;">
-          <a class="navbar-brand brand-logo mr-5" href="index.html"><img src="template/images/CARMEL (2).png" class="mr-2" alt="logo" /></a>
-          <a class="navbar-brand brand-logo-mini" href="index.html"><img src="template/images/logo-mini.svg" alt="logo" /></a>
+          <a class="navbar-brand brand-logo mr-5" href="index"><img src="template/images/CARMEL (2).png" class="mr-2" alt="logo" /></a>
+          <a class="navbar-brand brand-logo-mini" href="index"><img src="template/images/logo-mini.svg" alt="logo" /></a>
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end" style="background-color: #DFE0DA; ">
           <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
