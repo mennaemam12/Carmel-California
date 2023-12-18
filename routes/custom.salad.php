5e0d27d13020da1882d1be18fbb8694797aa6b30
Shahd Khaled <shahdksalah@gmail.com>
2023-12-18 23:37:23 +0200

    routes edit

--- routes/custom.salad.php
+++ routes/custom.salad.php
@@ -1,5 +1,15 @@
 <?php
 @session_start();
+
+$url = $_SERVER['REQUEST_URI'];
+
+// segments
+$segments = explode('/', $url);
+$lastSegment = trim(strtolower($segments[count($segments) - 1]));
+if (strpos($lastSegment, '?') !== false)
+    $lastSegment = strstr($lastSegment, '?', true);
+
+
 // Path: routes/salad-order
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     include 'controllers/ingredient.controller.php';
@@ -7,5 +17,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $ing->getSections();
     
     exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    switch ($lastSegment) {
+        case 'viewtotal':
+            include 'controllers/ingredient.controller.php';
+            IngredientController::getAjaxTotal();
+            exit();
+        case 'addtocart':
+
+        default:
+            include 'views/404.php';
+            exit();
+    }
 }
 ?>
