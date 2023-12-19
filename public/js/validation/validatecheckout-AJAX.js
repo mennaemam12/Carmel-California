fc82c976d6ffdea560db251c2ae76f9ebac9692f
Shahd Khaled <shahdksalah@gmail.com>
2023-12-19 23:41:29 +0200

    validate checkout form ajax

---
+++ public/js/validation/validatecheckout-AJAX.js
@@ -0,0 +1,26 @@
+
+    function validateForm(formData) {
+        // Use the $.ajax function
+        $.ajax({
+            url: "checkout/placeorder",
+            method: "POST",
+            contentType: 'application/json',
+            data: JSON.stringify(formData),
+            success: function(response) {
+            if (response.includes("successful")) {
+                try {
+                    
+                }catch (e) {
+                    console.error("Error caught:", e);
+                }
+            }
+            else{
+                document.getElementById('formerror').innerHTML = response;
+            }
+},
+            error: function(xhr, status, error) {
+                // Handle errors
+                console.error("Error with the request. Status code:", xhr.status);
+            }
+        });
+    }
