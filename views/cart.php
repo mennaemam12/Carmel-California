47e4c6cac37176cea9832fc55b3ba3b445f5c184
Shahd Khaled <shahdksalah@gmail.com>
2023-12-19 23:32:11 +0200

    view edits

--- views/cart.php
+++ views/cart.php
@@ -74,13 +74,13 @@ include 'projectFolderName.php';
 			<div class="container">
 				<div class="row">
 					<div class="col-md-12 ftco-animate">
-						<div class="cart-list">
+						<div class="cart-list" style='overflow-x: hidden;'>
 							<table class="table">
 								<thead class="thead-primary">
 								<tr class="text-center">
-									<th>&nbsp;</th>
-									<th>&nbsp;</th>
 									<th>Product</th>
+									<th>&nbsp</th>
+									<th>&nbsp</th>
 									<th>Price</th>
 									<th>Quantity</th>
 									<th>Total</th>
@@ -88,7 +88,7 @@ include 'projectFolderName.php';
 								</thead>
 								<tbody>
 									<?php
-									$i=0;
+									$i=0; 
 									$total=0;
 									foreach($items as $item){  
 									?> 
@@ -149,6 +149,8 @@ include 'projectFolderName.php';
 						</div>
 					</div>
 				</div>
+
+			<?php if(count($items)!=0): ?>
     		<div class="row justify-content-end">
     			<div class="col col-lg-3 col-md-6 mt-5 cart-wrap ftco-animate">
     				<div class="cart-total mb-3">
@@ -157,23 +159,23 @@ include 'projectFolderName.php';
     						<span>Subtotal</span>
     						<span><?=$total?> EGP</span>
     					</p>
-    					<p class="d-flex">
-    						<span>Delivery</span>
-    						<span>0.00 EGP</span>
-    					</p>
-    					<p class="d-flex">
-    						<span>Discount</span>
-    						<span>0.00 EGP</span>
-    					</p>
+    					
     					<hr>
     					<p class="d-flex total-price">
     						<span>Total</span>
     						<span><?=$total?> EGP</span>
     					</p>
     				</div>
-    				<p class="text-center"><a href="checkout" class="btn btn-primary py-3 px-4" style="background-color:#006a4d;">Proceed to Checkout</a></p>
+    				<p class="text-center"><a href="checkout"  class="btn btn-primary py-3 px-4" style="background-color:#006a4d;">Proceed to Checkout</a></p>
     			</div>
     		</div>
+			<?php else:?>
+				<div class="row justify-content-center ftco-animate " style="margin-top:50px">
+					<h2>No items to display.</h2>
+			</div>
+			<?php endif;?>
+
+
 		</div>
 	</section>
 
