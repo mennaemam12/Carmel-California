68c79d4000ef94996936812d58906bd210d16389
Shahd Khaled <shahdksalah@gmail.com>
2023-12-19 23:39:43 +0200

    checkout handling

---
+++ controllers/checkout.controller.php
@@ -0,0 +1,126 @@
+<?php
+require_once 'models/Cart.php';
+require_once 'models/User.php';
+require_once 'helpers/session.helper.php';
+include 'projectFolderName.php';
+include 'models/Discount.php';
+include 'models/BillingDetails.php';
+
+class CheckoutController
+{
+    private $checkoutModel;
+    public function __construct()
+    {
+    }
+
+    public static function viewCheckout()
+    {
+        $items = array();
+        $cartItems = array();
+        $user = new User;
+        $user->unserialize($_SESSION['user']);
+        foreach ($user->getCart() as $cartItem) {
+            $cart = new Cart;
+            $cart->unserialize($cartItem);
+            $cartItems[] = $cart;
+        }
+
+
+        foreach ($cartItems as $item) {
+            $items[] = Item::findItemByID($item->getItemType(), $item->getItemId());
+        }
+        $count = count($items);
+        $delivery = 50;
+        $total = 0;
+        foreach ($items as $item) {
+            $total += $item->Price;
+        }
+        
+
+
+        $details = BillingDetails::getUserBillingDetails($user->getID());
+        if($details){
+            $firstname = $details->FirstName;
+            $lastname = $details->LastName;
+            $area = $details->Area;
+            $street = $details->Street;
+            $building = $details->Building;
+            $floor = $details->Floor;
+            $apartment = $details->Apartment;
+            $postalcode = $details->Postalcode;
+        }
+        else{
+            $fullname = explode(' ', $user->getFullName());
+            $firstname = $fullname[0];
+            $lastname = $fullname[1];
+            $area = "";
+            $street = "";
+            $building = "";
+            $floor = "";
+            $apartment = "";
+            $postalcode = "";
+        }
+        include 'views/checkout.php';
+    }
+
+    public static function checkDiscount()
+    {
+        $code = $_POST['discountcode'];
+
+        $items = array();
+        $cartItems = array();
+        $user = new User;
+        $user->unserialize($_SESSION['user']);
+        foreach ($user->getCart() as $cartItem) {
+            $cart = new Cart;
+            $cart->unserialize($cartItem);
+            $cartItems[] = $cart;
+        }
+
+
+        foreach ($cartItems as $item) {
+            $items[] = Item::findItemByID($item->getItemType(), $item->getItemId());
+        }
+
+        $result = Discount::findDiscountByCode($code);
+        $discount = 0;
+        if ($result) {
+            foreach ($items as $item) {
+                if (strtolower($item->Category) === $result->category) {
+                    $discount = $result->percentage;
+                }
+            }
+        }
+
+        echo $discount;
+    }
+
+
+    public static function validateCheckout()
+    {
+        $json_data = file_get_contents("php://input");
+
+        $formdata = json_decode($json_data, true);
+
+        if(empty($formdata['firstname']) || empty($formdata['lastname']) || empty($formdata['area']) || 
+            empty($formdata['street']) || empty($formdata['building']) || empty($formdata['floor']) || empty($formdata['apartment'])){
+                echo "Please fill all required fields.";
+        }
+        else{
+            if($formdata['save']){
+                $user = new User;
+                $user->unserialize($_SESSION['user']);
+                $id = $user->getID();
+                $bill = new BillingDetails();
+                $bill->saveBillingDetails($id, $formdata);
+                if($bill){
+                    echo "saved successfully";
+                }
+                
+            }
+            echo "successful";
+        }
+
+    }
+}
+?>
